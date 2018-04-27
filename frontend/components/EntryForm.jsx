b6b0b0336efa6ffb25cda198ab2183c328b80eff
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2018-04-27 16:25:48 +0200

    Bump react-color

--- frontend/components/EntryForm.jsx
+++ frontend/components/EntryForm.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { CompactPicker } from 'react-color';
+import CompactPicker from 'react-color/lib/Compact';
 import PropTypes from 'prop-types';
 
 const FormTag = styled.form`
